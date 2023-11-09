ab1a5315a19d378b98704f5433eb3bddc8f5c017
diegomog-dev <diego.mogollon.camacho@gmail.com>
2023-11-09 16:16:57 -0500

    Feat: Separate project into layers to differentiate Models, Data Access and application-specific utilities.

---
+++ InventorySystemV7.Utilities/DS.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventorySystemV7.Utilities
+{
+    public static class DS
+    {
+    }
+}
