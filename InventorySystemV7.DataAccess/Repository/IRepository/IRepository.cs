9a7a1505739df0b995f33e023e7e6e441db7fbf2
diegomog-dev <diego.mogollon.camacho@gmail.com>
2023-11-14 17:56:38 -0500

    Feat: implementation of the Generic Repository pattern.

---
+++ InventorySystemV7.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventorySystemV7.DataAccess.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T> GetById(int id);      // Devuelve objeto T asociado al id que se envía como parametro
+        Task<IEnumerable<T>> GetAll(
+            Expression<Func<T, bool>> filter = null,                    // para poder filtrar la lista
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,   // para ordenar la lista
+            string addPropierties = null,                               // incluir propiedades adicionales al id
+            bool isTracking = true                                      // acceder al objeto y porderlo modificar
+            );
+        Task<T> GetFirst(
+            Expression<Func<T, bool>> filter = null,
+            string addPropierties = null,
+            bool isTracking = true
+            );
+        Task Add( T entity );
+        void Delete( T entity );
+        void DeleteRange(IEnumerable<T> entities );
+    }
+}
---
+++ InventorySystemV7.DataAccess/Repository/Repository.cs
@@ -0,0 +1,94 @@
+﻿using InventorySystemV7.DataAccess.Data;
+using InventorySystemV7.DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventorySystemV7.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+        internal DbSet<T> dbSet;
+        
+        public Repository(ApplicationDbContext db)
+        {
+            _context = db;
+            this.dbSet = _context.Set<T>();
+        }
+
+        public async Task Add(T entity)
+        {
+            await dbSet.AddAsync(entity);   // insert into Table
+        }
+
+        public async Task<T> GetById(int id)
+        {
+            return await dbSet.FindAsync(id);       // select * from (solo por Id)
+        }
+
+        public async Task<IEnumerable<T>> GetAll(Expression<Func<T, bool>> filter = null, 
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string addPropierties = null, bool isTracking = true)
+        {
+            IQueryable<T> query = dbSet;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);        // select * from where ...
+            }
+            if(addPropierties != null)
+            {
+                foreach(var item in addPropierties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(item);        // ejemplo "Categoria,Marca"
+                }
+            }
+            if(orderBy != null)
+            {
+                query = orderBy(query);
+            }
+            if (!isTracking)
+            {
+                query = query.AsNoTracking();
+            }
+            return await query.ToListAsync();
+        }
+
+        public async Task<T> GetFirst(Expression<Func<T, bool>> filter = null, string addPropierties = null, bool isTracking = true)
+        {
+            IQueryable<T> query = dbSet;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);        // select * from where ...
+            }
+            if (addPropierties != null)
+            {
+                foreach (var item in addPropierties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(item);        // ejemplo "Categoria,Marca"
+                }
+            }
+            if (!isTracking)
+            {
+                query = query.AsNoTracking();
+            }
+            return await query.FirstOrDefaultAsync();
+        }
+
+        public void Delete(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        public void DeleteRange(IEnumerable<T> entities)
+        {
+            dbSet.RemoveRange(entities);
+        }
+
+    }
+}
