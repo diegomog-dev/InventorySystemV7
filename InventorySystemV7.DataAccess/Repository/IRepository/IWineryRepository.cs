6cb980d08b061a9cb9ddfc5e2b73e2e7dc1eb7a6
diegomog-dev <diego.mogollon.camacho@gmail.com>
2023-11-14 21:05:11 -0500

    Feat: repository implementation for the Warehouse model

---
+++ InventorySystemV7.DataAccess/Repository/IRepository/IWineryRepository.cs
@@ -0,0 +1,14 @@
+﻿using InventorySystemV7.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventorySystemV7.DataAccess.Repository.IRepository
+{
+    public interface IWineryRepository : IRepository<Winery>
+    {
+        void Update(Winery winery);
+    }
+}
---
+++ InventorySystemV7.DataAccess/Repository/WineryRepository.cs
@@ -0,0 +1,34 @@
+﻿using InventorySystemV7.DataAccess.Data;
+using InventorySystemV7.DataAccess.Repository.IRepository;
+using InventorySystemV7.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventorySystemV7.DataAccess.Repository
+{
+    public class WineryRepository : Repository<Winery>, IWineryRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public WineryRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public void Update(Winery winery)
+        {
+            var wineryDB = _context.Wineries.FirstOrDefault( w => w.Id == winery.Id);
+            if (wineryDB != null)
+            {
+                wineryDB.Name = winery.Name;
+                wineryDB.Description = winery.Description;
+                wineryDB.State = winery.State;
+                _context.SaveChanges();
+            }
+        }
+    }
+}
