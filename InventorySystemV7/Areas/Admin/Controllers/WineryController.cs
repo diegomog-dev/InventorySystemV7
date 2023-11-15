094a210be5bf319e14b5eb31bf9f1242bf9d22e1
diegomog-dev <diego.mogollon.camacho@gmail.com>
2023-11-14 21:43:07 -0500

    Feat: implementation of the first controller for Winery.

---
+++ InventorySystemV7/Areas/Admin/Controllers/WineryController.cs
@@ -0,0 +1,29 @@
+﻿using InventorySystemV7.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventorySystemV7.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class WineryController : Controller
+    {        
+        private readonly IUnitWork _unitWork;
+
+        public WineryController(IUnitWork unitWork)
+        {
+            _unitWork = unitWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var all = await _unitWork.Winery.GetAll();
+            return Json(new {data = all});
+        }
+        #endregion
+    }
+}
