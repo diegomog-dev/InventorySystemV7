971614e1fbe259ab0c98d97f9ff85b29b4466aba
diegomog-dev <diego.mogollon.camacho@gmail.com>
2023-11-17 15:54:08 -0500

    feat: create views for model Category

---
+++ InventorySystemV7/wwwroot/js/category.js
@@ -0,0 +1,82 @@
+﻿let dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "language": {
+            "lengthMenu": "Mostrar _MENU_ Registros Por Pagina",
+            "zeroRecords": "Ningun Registro",
+            "info": "Mostrar page _PAGE_ de _PAGES_",
+            "infoEmpty": "no hay registros",
+            "infoFiltered": "(filtered from _MAX_ total registros)",
+            "search": "Buscar",
+            "paginate": {
+                "first": "Primero",
+                "last": "Último",
+                "next": "Siguiente",
+                "previous": "Anterior"
+            }
+        },
+        "ajax": {
+            "url": "/Admin/Category/GetAll"
+        },
+        "columns": [
+            { "data": "name", "width": "20%" },
+            { "data": "description", "width": "40%" },
+            {
+                "data": "state",
+                "render": function (data) {
+                    if (data == true) {
+                        return "Activo";
+                    } else {
+                        return "Inactivo";
+                    }
+                },"width": "20%"
+            },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                        <div class="text-center">
+                            <a href="/Admin/Category/Upsert/${data}" class="btn btn-success text-white" style="cursor: pointer">
+                                <i class="bi bi-pencil-square"></i>
+                            </a>
+                            <a onclick=Delete("/Admin/Category/Delete/${data}") class="btn btn-danger text-white" style="cursor: pointer">
+                                <i class="bi bi-trash3-fill"></i>
+                            </a>
+                        </div>
+                    `;
+                },
+                "width": "20%"
+            },
+        ]
+    });
+}
+
+function Delete(url) {
+    swal({
+        title: "Esta seguro de Eliminar la Categoria?",
+        text: "Este registro no se podra recuperar.",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+    }).then((dele) => {
+        if (dele) {
+            $.ajax({
+                type: "POST",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    } else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
+    });
+}
